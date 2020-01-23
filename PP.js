2269fc883a02eb43002c531882c183d109b12199
mohan-kc <dhanbdr787@gmail.com>
2020-01-23 16:01:45 +0545

    asd

---
+++ PP.js
@@ -0,0 +1,5 @@
+const userName=document.getElementById('name').value;
+console.log(`YOu entered ${userName}`);
+document.querySelector(".myForm").addEventListener('click',e =>{
+    e.preventDefault();
+});
